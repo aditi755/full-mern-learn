296f9206cf12ad0f9f64dcf66e69f2907ed62ccd
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-04-14 15:21:29 +0530

    complete backend added

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -4,6 +4,9 @@ const express = require('express');
 const router = express.Router(); // Instantiate the router
 
 router.get('/', booksController.getAllBooks)
-router.post('/', booksController.addBook)
+router.post('/', booksController.addBook) 
+router.get('/:id', booksController.getById)
+router.put('/:id', booksController.updateBook ) //http://localhost:5000/books/661ad2d99352a93e1c08d00e
+router.delete('/:id', booksController.deleteBook)
 
 module.exports = router
